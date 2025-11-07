693cb9708837963d720e7df95e07addca8f9bca0
krishna2700 <krishnaruparelia0207@gmail.com>
2025-11-07 09:08:47 +0530

    server and db setup

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Server is ready!"));
+
+const PORT = process.env.PORT || 5000;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect DB:", err);
+  });
