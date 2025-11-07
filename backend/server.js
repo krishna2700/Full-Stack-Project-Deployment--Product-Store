0d4da231374b46d0c553ad6a55c4295dacb66c2c
krishna2700 <krishnaruparelia0207@gmail.com>
2025-11-07 22:39:03 +0530

    backend api,models,db connection

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
-import express from "express";
 import dotenv from "dotenv";
+import express from "express";
 import { connectDB } from "./config/db.js";
+import ProductRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json()); // to parse JSON request bodies
 
 app.get("/", (req, res) => res.send("Server is ready!"));
 
 const PORT = process.env.PORT || 5000;
 
+app.use("/api/products", ProductRoutes);
 connectDB()
   .then(() => {
     app.listen(PORT, () => {
