95ec7005189cd8cb6b23815e877dcc89de37e693
krishna2700 <krishnaruparelia0207@gmail.com>
2025-11-08 10:05:00 +0530

    pages, componet setup navbar ui build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
-import { Button } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import CreatePage from "./pages/CreatePage";
+import HomePage from "./pages/HomePage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Button>Click</Button>
-    </>
+    <Box minH={"100vh"}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
   );
 }
 
