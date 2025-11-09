d329825eb6573786be0af5f27fcd963b682917eb
krishna2700 <krishnaruparelia0207@gmail.com>
2025-11-09 10:41:12 +0530

    build and deploy setup

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,34 @@
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import { connectDB } from "./config/db.js";
 import ProductRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
-app.use(express.json()); // to parse JSON request bodies
+app.use(express.json());
 
-app.get("/", (req, res) => res.send("Server is ready!"));
-
-const PORT = process.env.PORT;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use("/api/products", ProductRoutes);
+
+if (process.env.NODE_ENV !== "production") {
+  app.get("/", (req, res) => res.send("Server is ready!"));
+}
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
 connectDB()
   .then(() => {
     app.listen(PORT, () => {
